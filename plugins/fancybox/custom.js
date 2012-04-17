e28f6044c588ff9e4ce31d58c17056af001c13d7
registero <registero@arcor.de>
2012-04-17 17:50:36 +0200

    2012/04/17

--- plugins/fancybox/custom.js
+++ plugins/fancybox/custom.js
@@ -5,9 +5,33 @@
 
 // activate fancybox for all a with class fancybox
 	$(document).ready(function() {
+		
+		function formatTitle(title, currentArray, currentIndex, currentOpts) {
+		    return '<div id="tip7-title"><span><a href="javascript:;" onclick="$.fancybox.close();"><img src="/data/closelabel.gif" /></a></span>' + (title && title.length ? '<b>' + title + '</b>' : '' ) + 'Image ' + (currentIndex + 1) + ' of ' + currentArray.length + '</div>';
+		}
+		
+		
+		
 		$(".fancybox").fancybox({
 			closeBtn: true,
 			loop: true,
-			
+			helpers : {
+                title : {
+                    type : 'inside'
+                }
+
+            },
+            beforeShow : function(opt) {
+	            if(this.element.title==''){
+	            	
+	            	socialbar = this.element.parentElement.children[1].innerHTML;
+	                this.element.title='Image '+ (this.index + 1) + ' / ' + this.group.length + socialbar;
+	                this.title=this.element.title;
+	            }
+	        }
+
+
 		});
+		
+		
 	});
--- plugins/tools/custom.js
+++ plugins/tools/custom.js
@@ -3,11 +3,20 @@
  */
 
 
-// activate scrollavle 
+
 	$(document).ready(function() {
 
 		$(function() {
 		  // initialize scrollable
 		  $(".scrollable").scrollable();
 		});
+		
+		
+		$(function() {
+		  // initialize scrollable
+		  $(".scrollable2").scrollable();
+		});
+		
+		
+		$("img[rel]").overlay();
 	});
